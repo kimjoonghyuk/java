d3b91c87fdd861a504e7b873a78178f89b606325
Andy <Andy@192.168.1.159>
2022-09-25 17:27:03 +0900

    0101

--- src/com/tset/ch06/CarExample.java
+++ src/com/tset/ch06/CarExample.java
@@ -1,10 +1,38 @@
-package com.tset.ch06;
+package com.test.ch06;
 
-public class CarExample {
+public class Carexample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int price;
+		String company = "";
+		String model = "";
+		
+		Car car = new Car();
+		car.setCompany("현대");
+		car.setModel("쏘나타");
+		
+		Car car2 = new Car("기아");
+		Car car3 = new Car("테슬라", "모델s", "블랙");
+		car3.setColor("레드");
+		car3.setMaxSpeed(120);
+		car3.setDueDate(args);
+		
+		/*String[] dueDateArr = new String[7]; dueDateArr[0]="06-01;
+		 */
+		
+		
+		String[] dueDateArr = {"06-01", "07-01", "08-01", "09-01", "10-01", "11-01", "12-01"};
+		
+		System.out.println(car.toString());
+		System.out.println(car2.toString());
+		System.out.println(car3.toString());
+		
+		car3.setMaxSpeed(200);
+		System.out.println(car3.toString());
+		
+		car3.setMaxSpeed(300);
+		System.out.println(car3.toString());
+		
 	}
 
 }
